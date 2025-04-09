abefe1fc70d6e8795114ddb9962cb5713c66e303
Guy Riff <guy22riff@gmail.com>
2025-04-09 21:03:08 +0300

    updated project

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import pygame
-from constants import*
+from constants import *
+from player import Player
+
+
  
-def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
+def main():   
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
+        # Handle events
         for event in pygame.event.get():
-               if event.type == pygame.QUIT:
-                    return
-               
-
+            if event.type == pygame.QUIT:
+                return
+        
+        # Calculate dt for this frame
+        dt = clock.tick(60) / 1000
+        
+        # Update game state
+        player.update(dt)
+        
+        # Render
         screen.fill("black")
+        player.draw(screen)            
         pygame.display.flip()
-
-        dt =clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
